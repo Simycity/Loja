280be7bf7e71484c92f292fd125a5e13e81a6c34
VITOR HUGO ZAMBORLINI MASIOL <v.filho@labcetec.net>
2025-10-14 21:06:40 -0300

    Finalização da tela de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,49 +16,88 @@ import { useVerificaLogin } from "../hooks/useUsusarios";
 // Importandoa função useForm para verificar do pacote hook-form
 import { useForm } from "react-hook-form";
 
-const Login = () => {
+// Importa useState para tratar de variáveis
+import { useState } from 'react';
 
+// Importação do Navigate para transitar entre as páginas
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
   // register = cria um objeto com os valores retirados dos inputs
   // handleSubmit = envia os dados formulário, caso dê erro ou sucesso
   // formState {errors} = objeto que guarda uma lista de erros que aconteceram na tentativa do envio
-  const { register, handleSubmit, formState: {errors} } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // Variavel para tratar a classe do alerta
+  const [alertaClasse, setAlertaClasse] = useState("d-none");
+
+  // Usando apenas a função verificaLogin, que importei do hook
+  const { verificaLogin } = useVerificaLogin()
+
+  const navigate = useNavigate();
 
   // Caso o envio dê certo
   // data = um objeto com todas as informações preenchidas nos campos do formulário
   const onSubmit = (data) => {
     console.log("Dados enviados:", data);
-    
-  }
+
+    // Cria uma variável para armazenar a resposta completa que veio da função
+    const resposta = verificaLogin(data);
+
+    if(resposta === "Login efetuado com sucesso"){
+      alert(resposta)
+      navigate("/home");
+    } else {
+      setAlertaClasse("my-3 w-75 mx-auto");
+    }
+  };
 
   // Caso o envio dê certo
-   // errors = um objeto com todos os erros do envio
+  // errors = um objeto com todos os erros do envio
   const onError = (errors) => {
     console.log("Erros:", errors);
-    
-  }
-
-
+  };
 
   return (
     <Container className="justify-content-center align-content-center min-vh-100">
       {/* Linhas para os campos de login e ícone */}
-      <Row className="bg-black">
+      <Row className="bg-success">
         {/* Coluna com ícone da página */}
         <Col>
-        {/* Ícone de Login */}
-          <SiRiotgames style={{fontSize:"500px", color:"red"}}/>
+          {/* Ícone de Login */}
+          <SiRiotgames style={{ fontSize: "500px", color: "red" }} />
         </Col>
 
         {/* Coluna com os campos de login */}
         <Col className="d-flex flex-column">
-          <Form style={{ with:"75%", margin: "auto", textAlign: "center" }}>
+          <Form 
+          style={{ width: "75%", margin: "auto", textAlign: "center" }}
+          // Utilizar o evento onSubmit para envio do formulário e o handleSubmit vindo do hookForm
+          onSubmit={handleSubmit( onSubmit, onError)}>
+
             {/* Caixinha de Email */}
             <FloatingLabel
               controlId="inputEmail"
               label="Digite seu Email"
               className="mb-5"
             >
-              <Form.Control type="email" />
+              <Form.Control 
+                type="email"
+                {...register("email", {
+                  required: "O email é obrigatório",
+                  pattern: {
+                    value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i,
+                    message: "Email inválido"
+                  },
+                  validate: (value) => value.includes("@") || "O email deve posuir um @"
+                })}/>
+                {errors.email && (<p>{errors.email.message}</p>)}
+              {/* Fim da caixinha de email */}
+
             </FloatingLabel>
             {/* Fim da caixinha de Email */}
 
@@ -68,7 +107,13 @@ const Login = () => {
               label="Senha"
               className="mb-5"
             >
-              <Form.Control type="password" />
+              <Form.Control 
+              type="password" 
+                {...register("senha", {
+                    required: "A senha é obrigatória"
+                  })}
+              />
+              {errors.senha && (<p className="error">{errors.senha.message}</p>)}
             </FloatingLabel>
             {/* Fim da caixinha de Senha */}
 
@@ -77,10 +122,12 @@ const Login = () => {
               Login
             </Button>
 
-            {/* Alerta de Erro */}
-            <Alert variant="danger" className="my-3 w-75 mx-auto">
-              Usuario ou senha estão inválidos
+            {/* Alerta, caso haja algum erro*/}
+            {/* <Alert variant="danger" className="my-3 w-75 mx-auto"> */}
+            <Alert variant="danger" className={alertaClasse}>
+              Email ou senha inválidos!
             </Alert>
+            {/* Fim do alerta de email ou senha inválidos */}
           </Form>
         </Col>
       </Row>
