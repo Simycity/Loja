90525c2bd801248f17164c24de8cb7e5c3be28b9
VITOR HUGO ZAMBORLINI MASIOL <v.filho@labcetec.net>
2025-10-28 19:05:37 -0300

    Tela de cadastro de Produtos e adição de hook

--- src/MyRouter.jsx
+++ src/MyRouter.jsx
@@ -1,40 +1,61 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
 
-// Importação das páginas principais 
-import App from "./App.jsx"
-import PaginaErro from "./pages/PaginaErro.jsx"
-import Login from "./pages/Login.jsx"
-import RotasProtegidas from "./pages/RotasProtegidas.jsx"
-import Home from "./pages/Home.jsx"
+// Importação das páginas principais
+import App from "./App.jsx";
+import PaginaErro from "./pages/PaginaErro.jsx";
+import Login from "./pages/Login.jsx";
+import RotasProtegidas from "./pages/RotasProtegidas.jsx";
+import Home from "./pages/Home.jsx";
 
+// Importação páginas produto
+import VerProdutos from "./pages/Produtos/VerProdutos.jsx";
+import CadastrarProduto from "./pages/Produtos/CadastrarProduto.jsx";
+import EditarProduto from "./pages/Produtos/EditarProduto.jsx";
 
 const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <App />,
-        errorElement: <PaginaErro />,
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <PaginaErro />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <RotasProtegidas />,
+    errorElement: <PaginaErro />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "produtos",
         children: [
-            {
-                index: true,
-                element: <Login />
-            },
-            {
-                path:"login",
-                element: <Login />
-            }
-        ]
-    },
-    {
-        path: "/",
-        element: <RotasProtegidas />,
-        errorElement: <PaginaErro />,
-        children:[
-            {
-                path: "home",
-                element: <Home />
-            }
-        ]
-    }
- ])
+          {
+            index: true,
+            element: <VerProdutos />,
+          },
+          {
+            path: "cadastrar",
+            element: <CadastrarProduto />,
+          },
+          {
+            path: "editar/:id",
+            element: <EditarProduto />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
- export default router
+export default router;
